b92f411e35b08c2b8ac408068cc55ac4a37d03b4
Larry Hudson <larryhudson@hey.com>
2021-03-28 11:04:32 +1100

    add tagline

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
     'baseURL': process.env.ELEVENTY_ENV == 'production' ? 'https://www.larryhudson.io' : 'http://localhost:8080',
     'title': 'Larry Hudson',
+    'tagline': 'Writing about making websites',
     'description': 'Larry Hudson is a web developer based in Melbourne, Australia, focusing on simplicity and accessibility.',
     'twitter': 'larryhudsondev'
 }
