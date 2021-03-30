a0d5ffeb6b46e159d1658c7472b6fe8cf8cda9de
Larry Hudson <larryhudson@hey.com>
2021-03-30 21:56:20 +1100

    styling

--- src/_utils/replace-images.js
+++ src/_utils/replace-images.js
@@ -1,31 +1,55 @@
 const Image = require('@11ty/eleventy-img');
 
+async function getHtmlForImg(url) {
+  let metadata = await Image(url, {
+    widths: [480, 768, 992],
+    formats: ["avif", "jpeg"],
+    outputDir: '_site/img',
+  });
+
+  return Image.generateHTML(metadata, {
+    alt: '',
+    sizes: [480, 768, 992]
+  })
+}
+
 async function replaceImagesInContent(content) {
 
     var re = /<img.*?src=\"(.*?)\"(.*?)\>/g;
     var match;
     var workingString = content;
 
+    var imagesToReplace = []
+
+
+    // Get matches and add them to array
     while ((match = re.exec(content)) != null) {
 
         [imgTag, imgUrl] = match;
 
-        let metadata = await Image(imgUrl, {
-          widths: [480, 768, 992],
-          formats: ["avif", "jpeg"],
-          outputDir: '_site/img',
-        });
-
-        var imgHtml = Image.generateHTML(metadata, {
-          alt: '',
-          sizes: [480, 768, 992]
+        imagesToReplace.push({
+          tag: imgTag,
+          url: imgUrl
         })
-
-        workingString = workingString.replace(
-            imgTag,
-            imgHtml
-        )
     }
+
+    // Add processed image HTML to image tags
+    var replacements = imagesToReplace.map(async ({tag, url}) => {
+      var html = await getHtmlForImg(url)
+      return {
+        tag,
+        url,
+        html
+      }
+    })
+
+    // Replace image tags with new image HTML
+    replacements.forEach(r => {
+      workingString = workingString.replace(
+        r.tag,
+        r.html
+    )
+    })
 
     return workingString
 }
