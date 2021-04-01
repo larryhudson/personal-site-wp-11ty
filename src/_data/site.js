a3a0dc07a669d554844d06d29a59515061e1fc05
Larry Hudson <larryhudson@hey.com>
2021-04-01 18:24:08 +1100

    fart head

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,6 @@ module.exports = {
     'baseURL': process.env.ELEVENTY_ENV == 'production' ? 'https://www.larryhudson.io' : 'http://localhost:8080',
     'title': 'Larry Hudson',
     'tagline': 'Writing about making websites',
-    'description': 'Larry Hudson is a web developer based in Melbourne, Australia, focusing on simplicity and accessibility.',
+    'description': 'Larry Hudson is a web developer based in Melbourne, Australia, focusing on simplicity and accessibility. fart head',
     'twitter': 'larryhudsondev'
 }
